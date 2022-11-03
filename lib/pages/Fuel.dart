a53e7329c20e3bbe003b01b1b7c86a0597c340da
fiftekhar <iftekhar.f.19@gmail.com>
2022-11-03 00:18:59 -0400

    redid the google maps

--- lib/pages/Fuel.dart
+++ lib/pages/Fuel.dart
@@ -1,13 +1,9 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:motore/services/auth.dart';
 import 'package:geolocator/geolocator.dart';
 import 'dart:async';
 
+LatLng startingLocation = LatLng(40.7678, -73.9645);
 class Fuel extends StatefulWidget {
   const Fuel({Key? key}) : super(key: key);
 
@@ -16,114 +12,43 @@ class Fuel extends StatefulWidget {
 }
 
 class _FuelState extends State<Fuel> {
-  late LatLng currentLatLng;
-  // late LatLng currentLatLng = const LatLng(48.8566, 2.3522);
-  final Completer<GoogleMapController> _controller = Completer();
-  Future<void> _determinePosition() async {
-    LocationPermission permission;
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.deniedForever) {
-        return Future.error('Location permissions denied!');
-      }
-    } else {
-      throw Exception(Error);
-    }
-    
-    Position position = await Geolocator.getCurrentPosition();
-    setState(() {
-      currentLatLng = LatLng(position.latitude, position.longitude);
-      _goToCurrentLocation();
-    });
-    return;
-  }
 
-  @override
-  void initState() {
-    super.initState();
-  }
+  late GoogleMapController mapController;
 
-  @override
+  @override 
   Widget build(BuildContext context) {
-    // print("Current Location: " + currentLatLng.latitude.toString() + " " + currentLatLng.longitude.toString());
-    return MaterialApp(
-      home: Scaffold(
-        body: GoogleMap(
-          mapType: MapType.normal,
-          initialCameraPosition: CameraPosition(target: currentLatLng, zoom: 5),
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete(controller);
-          },
-          markers: <Marker>{
-              Marker(
-                draggable: true,
-                markerId: const MarkerId("1"),
-                position: currentLatLng,
-                icon: BitmapDescriptor.defaultMarker,
-                infoWindow: const InfoWindow(
-                  title: 'My Location',
-                ),
-              )
-            },
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: startingLocation,
+          zoom: 12,
         ),
+        onMapCreated: (controller) {
+          mapController = controller;
+        },
 
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: _goToCurrentLocation,
-          label: const Text('Home'),
-          icon: const Icon(Icons.home),
-        ),
-      ),
+      )
     );
   }
 
-  Future<void> _goToCurrentLocation() async {
-    await _determinePosition();
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: currentLatLng, zoom: 3)));
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error("Location services are disabled.");
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+
+      if (permission == LocationPermission.denied) {
+        return Future.error("Location permissions are denied.");
+      }
+    }
+
+    return await Geolocator.getCurrentPosition();
   }
-  // late GoogleMapController mapController;
-
-  // final LatLng _center = const LatLng(45.521563, -122.677433);
-
-  // void _onMapCreated(GoogleMapController controller) {
-  //   mapController = controller;
-  // }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp(
-  //     home: Scaffold(
-  //       appBar: AppBar(
-  //         title: const Text(
-	// 				'Nearby Gas Stations',
-	// 				style: TextStyle(
-	// 					color: Color.fromARGB(255, 255, 255, 255)
-	// 				),
-	// 			),
-	// 			centerTitle: true,
-	// 			elevation: 0,
-	// 			flexibleSpace: Container(
-	// 				decoration: const BoxDecoration(
-	// 					gradient: LinearGradient(
-	// 						begin: Alignment.bottomLeft,
-	// 						end: Alignment.bottomRight,
-	// 						colors: [
-	// 							Color(0xFF15AAAFF),
-	// 							Color(0xFFADF7F2),
-	// 						]
-	// 					),
-	// 				),
-	// 			),
-  //       ),
-  //       body: GoogleMap(
-  //         onMapCreated: _onMapCreated,
-  //         initialCameraPosition: CameraPosition(
-  //           target: _center,
-  //           zoom: 11.0,
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
 }
