0bb6fa00af8c0c4a5bb74f24df1563fcd96004b7
fiftekhar <iftekhar.f.19@gmail.com>
2022-11-05 22:19:53 -0400

    removed unused imports from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:motore/models/myuser.dart';
-import 'package:motore/navbar.dart';
 import 'package:motore/pages/CarInfo.dart';
-import 'package:motore/pages/history_fourth.dart';
-import 'package:motore/pages/createHistoryEntry.dart';
-import 'package:motore/pages/history_second.dart';
-import 'package:motore/pages/History.dart';
-import 'package:motore/screens/authenticate/login.dart';
-import 'package:motore/screens/introPage.dart';
-import 'package:motore/screens/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:motore/services/auth.dart';
-import 'package:provider/provider.dart';
-import 'package:motore/splash.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 Future<void> main() async {
