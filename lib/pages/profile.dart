580074e2657c578d5ff60fd70ee1aeb012625a09
Navait Kumar <navaitk031@gmail.com>
2022-11-09 13:29:15 -0500

    added dropdown in Profile pg

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -3,6 +3,13 @@ import 'package:motore/services/auth.dart';
 import 'package:motore/pages/createCarProfile.dart';
 import 'package:motore/services/auth.dart';
 
+const List<String> list = <String>[
+  ' Car One',
+  ' Car Two',
+  ' Car Three',
+  ' Car Four',
+];
+
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
   @override
@@ -80,7 +87,31 @@ class _ProfileState extends State<Profile> {
                   ]),
             ),
           ),
-          SizedBox(height: 60),
+          SizedBox(height: 10),
+          DropdownButton<String>(
+            //value: dropdownValue,
+            icon: const Icon(Icons.arrow_downward),
+            elevation: 16,
+            style: const TextStyle(color: Colors.blue),
+            underline: Container(
+              height: 2,
+              color: Colors.red,
+            ),
+            onChanged: (String? value) {
+              // This is called when the user selects an item.
+              // setState(() {
+              //   dropdownValue = value!;
+              // }
+              //);
+            },
+            items: list.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+          SizedBox(height: 30),
           Row(
             children: [
               ProfileRow(title: 'Name', value: 'John Doe'),
