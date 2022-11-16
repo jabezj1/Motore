c256f7c6308ea59b6542259f293c137e745042d0
Navait Kumar <navaitk031@gmail.com>
2022-11-16 13:07:48 -0500

    converted all widgets related to carpart pg to stateful widgets

--- lib/pages/Tab.dart
+++ lib/pages/Tab.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
-class MyTab extends StatelessWidget {
+class MyTab extends StatefulWidget {
   final String iconPath;
 
   const MyTab({super.key, required this.iconPath});
 
+  @override
+  State<MyTab> createState() => _MyTabState();
+}
+
+class _MyTabState extends State<MyTab> {
   @override
   Widget build(BuildContext context) {
     return Tab(
@@ -14,7 +19,7 @@ class MyTab extends StatelessWidget {
         decoration: BoxDecoration(
             color: Colors.grey[200], borderRadius: BorderRadius.circular(6)),
         child: Image.asset(
-          iconPath,
+          widget.iconPath,
           color: Colors.grey[600],
         ),
       ),
