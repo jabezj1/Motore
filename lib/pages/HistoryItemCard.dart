97db45ba33be43063c9fa02906a6166976b97fdf
Navait Kumar <navaitk031@gmail.com>
2022-11-16 18:32:14 -0500

    fixed overflow issue on text

--- lib/pages/HistoryItemCard.dart
+++ lib/pages/HistoryItemCard.dart
@@ -1,63 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:motore/pages/HistoryItem.dart';
 
 class HistoryItemCard extends StatelessWidget {
-    final HistoryItem _historyItem;
-	const HistoryItemCard(this._historyItem, {super.key});
+  final HistoryItem _historyItem;
+  const HistoryItemCard(this._historyItem, {super.key});
 
-    @override
-    Widget build(BuildContext context) {
-		return Container(
-			child: Card(
-				child: Padding(
-					padding: const EdgeInsets.all(12.0),
-					child: Column(
-						children: [
-							Row(
-								mainAxisAlignment: MainAxisAlignment.spaceBetween, // forces children in row to split left & right
-								children: [
-									Column(
-										children: [
-											Padding(
-												padding: const EdgeInsets.only(bottom: 10.0),
-												child: Text(_historyItem.title!.toUpperCase().toString()) // title of maintenance
-											),
-											Padding(
-												padding: const EdgeInsets.only(top: 10.0, left: 20.0),
-												child: Text(_historyItem.notes.toString()) // notes that go under the title
-											),
-											Padding(
-												padding: const EdgeInsets.only(top: 10),
-												child: Row(
-													children: [
-														const Text("LOCATION: "),
-														Text(_historyItem.location.toString())
-													],
-												),
-											)
-										],
-									),
-									Column(
-										children: [
-											Padding(
-												padding: const EdgeInsets.only(bottom: 10.0),
-												child: Row(
-													children: [
-														const Text("\$"),
-														Text(_historyItem.cost.toString()), // cost of the maintenance, far right side
-													],
-												)
-											),
-										],
-									)
-								],
-							),
-						],
-					)
-				),
-			),
-		);
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Card(
+        child: Container(
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(15.0)),
+          ),
+          height: 200,
+          //   color: Color.fromARGB(255, 251, 240, 240),
+          child: Container(
+            child: Padding(
+                padding: const EdgeInsets.all(1.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment
+                          .spaceBetween, // forces children in row to split left & right
+                      children: [
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Padding(
+                                  padding: const EdgeInsets.only(top: 10.0),
+                                  child: Text(_historyItem.title!
+                                      .toUpperCase()
+                                      .toString()) // title of maintenance
+                                  ),
+                              Padding(
+                                  padding: EdgeInsets.all(20),
+                                  child: Text(
+                                    _historyItem.notes.toString(),
+                                    textAlign: TextAlign.left,
+                                  ) // notes that go under the title
+                                  ),
+                              Padding(
+                                padding: EdgeInsets.all(20),
+                                child: Row(
+                                  children: [
+                                    const Text("LOCATION: "),
+                                    Text(_historyItem.location.toString())
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Padding(
+                                padding: const EdgeInsets.only(right: 10.0),
+                                child: Row(
+                                  children: [
+                                    const Text("\$"),
+                                    Text(_historyItem.cost
+                                        .toString()), // cost of the maintenance, far right side
+                                  ],
+                                )),
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                )),
+          ),
+        ),
+      ),
+    );
+  }
 }
