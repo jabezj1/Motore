ab920992d6c7e1b371df3470fb0798d151d0e4a0
fiftekhar <iftekhar.f.19@gmail.com>
2022-12-05 15:33:13 -0500

    createCarProfile readability changes

--- lib/pages/createCarProfile.dart
+++ lib/pages/createCarProfile.dart
@@ -27,152 +27,141 @@ class StateCreateCarProfile extends State<CreateCarProfile> {
 	@override
 	Widget build(BuildContext context) {
 		return Container(
-		decoration: const BoxDecoration(
-			gradient: LinearGradient(
-				begin: Alignment.bottomLeft,
-				end: Alignment.bottomRight,
-				colors: [
-				Color(0xff15aaaff),
-				Color(0xFFADF7F2),
-				]),
-		),
-		child: Scaffold(
-			backgroundColor: Colors.transparent,
-			appBar: AppBar(
-				title: const Text(
-				'Add a Car',
-				style: TextStyle(color: Color.fromARGB(255, 5, 132, 235)),
-				),
-				centerTitle: true,
-				elevation: 0,
-				flexibleSpace: Container(
-				decoration: const BoxDecoration(
-					gradient: LinearGradient(
-						begin: Alignment.bottomLeft,
-						end: Alignment.bottomRight,
-						colors: [Color(0xFF15AAAFF), Color(0xFFADF7F2)]),
-				),
+			decoration: const BoxDecoration(
+				gradient: LinearGradient(
+					begin: Alignment.bottomLeft,
+					end: Alignment.bottomRight,
+					colors: [
+					Color(0xff15aaaff),
+					Color(0xFFADF7F2),
+					]
 				),
 			),
-			//backgroundColor: Color.fromARGB(255, 150, 206, 232),
-			body: Container(
-				margin: const EdgeInsets.only(top: 20),
-				child: SingleChildScrollView(
-				child: Column(children: [
-					const Text(
-					"Enter details about your vehicle with this form.",
-					textAlign: TextAlign.center,
-					style: TextStyle(
-						fontSize: 15,
-						fontWeight: FontWeight.bold,
+			child: Scaffold(
+				backgroundColor: Colors.transparent,
+				appBar: AppBar(
+					title: const Text(
+						'Add a Car',
+						style: TextStyle(color: Color.fromARGB(255, 5, 132, 235)),
 					),
-					),
-					const SizedBox(
-					height: 20,
-					),
-					EnterCarInfo(hintText: "Make", controller: carMakeController),
-					EnterCarInfo(hintText: "Model", controller: carModelController),
-					EnterCarInfo(
-						hintText: "Model Year", controller: carModelYearController),
-					EnterCarInfo(
-						hintText: "Model Trim", controller: carTrimController),
-					EnterCarInfo(
-						hintText: "Mileage", controller: carMileageController),
-					EnterCarInfo(
-						hintText: "Car Nickname",
-						controller: carNicknameController),
-					const SizedBox(
-					height: 30,
-					),
-					Container(
-						height: 50,
-						width: 280,
-						padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-						child: ElevatedButton(
-							style: ElevatedButton.styleFrom(
-							backgroundColor: Colors.blue,
-							side: const BorderSide(width: 4.0),
-							shape: RoundedRectangleBorder(
-								borderRadius: BorderRadius.circular(30)),
+					centerTitle: true,
+					elevation: 0,
+					flexibleSpace: Container(
+						decoration: const BoxDecoration(
+							gradient: LinearGradient(
+								begin: Alignment.bottomLeft,
+								end: Alignment.bottomRight,
+								colors: [Color(0xFF15AAAFF), Color(0xFFADF7F2)]
 							),
-							child: const Text('Add Vehicle',
-								style:
-									TextStyle(fontSize: 20, color: Colors.black)),
-							onPressed: () {
-							print({
-								"title": carMakeController.text,
-								"date": carModelController.text,
-								"cost": carModelYearController.text,
-								"location": carTrimController.text,
-								"notes": carMileageController.text,
-								"nickname": carNicknameController.text
-							});
-
-							users
-								.doc(FirebaseAuth.instance.currentUser?.email)
-								.collection("cars")
-								.add({
-									"make": carMakeController.text,
-									"model": carModelController.text,
-									"year": carModelYearController.text,
-									"trim": carTrimController.text,
-									"miles": carMileageController.text,
-									"nickname": carNicknameController.text
-								})
-								//todo fix fields
-								.then((value) => {
-										curr_car = value.id
-										// users
-										//         .doc("iftekhar.f.19@gmail.com")
-										//         .collection("cars")
-										//         .doc(value.id)
-										//         .collection("history")
-										//         .add({
-										//       "Title": "",
-										//       "Date": DateTime.now(),
-										//       "Cost": "",
-										//       "Location": "",
-										//       "Notes": ""
-									})
-								.catchError((e) => print(e));
-
-								Navigator.push(
+						),
+					),
+				),
+				//backgroundColor: Color.fromARGB(255, 150, 206, 232),
+				body: Container(
+					margin: const EdgeInsets.only(top: 20),
+					child: SingleChildScrollView(
+						child: Column(
+							children: [
+								const Text(
+									"Enter details about your vehicle with this form.",
+									textAlign: TextAlign.center,
+									style: TextStyle(
+										fontSize: 15,
+										fontWeight: FontWeight.bold,
+									),
+								),
+								const SizedBox(height: 20),
+								EnterCarInfo(hintText: "Make", controller: carMakeController),
+								EnterCarInfo(hintText: "Model", controller: carModelController),
+								EnterCarInfo(hintText: "Model Year", controller: carModelYearController),
+								EnterCarInfo(hintText: "Model Trim", controller: carTrimController),
+								EnterCarInfo(hintText: "Mileage", controller: carMileageController),
+								EnterCarInfo(hintText: "Car Nickname", controller: carNicknameController),
+								const SizedBox(height: 30),
+								Container(
+									height: 50,
+									width: 280,
+									padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+									child: ElevatedButton(
+										style: ElevatedButton.styleFrom(
+											backgroundColor: Colors.blue,
+											side: const BorderSide(width: 4.0),
+											shape: RoundedRectangleBorder(
+												borderRadius: BorderRadius.circular(30)
+											),
+										),
+										child: const Text(
+											'Add Vehicle',
+											style: TextStyle(fontSize: 20, color: Colors.black)
+										),
+										onPressed: () {
+											users
+												.doc(FirebaseAuth.instance.currentUser?.email)
+												.collection("cars")
+												.add({
+													"make": carMakeController.text,
+													"model": carModelController.text,
+													"year": carModelYearController.text,
+													"trim": carTrimController.text,
+													"miles": carMileageController.text,
+													"nickname": carNicknameController.text
+												})
+											.then((value) => {
+												curr_car = value.id
+													// users
+													//         .doc("iftekhar.f.19@gmail.com")
+													//         .collection("cars")
+													//         .doc(value.id)
+													//         .collection("history")
+													//         .add({
+													//       "Title": "",
+													//       "Date": DateTime.now(),
+													//       "Cost": "",
+													//       "Location": "",
+													//       "Notes": ""
+											}).catchError((e) => print(e));
+											
+											Navigator.push(
 												context, 
 												MaterialPageRoute(
 													builder: (context) => const Profile()
 												)
 											);
-							})),
-				]),
-				),
-			)),
+										}
+									)
+								),
+							]
+						),
+					),
+				)
+			),
 		);
 	}
 }
 
 class EnterCarInfo extends StatelessWidget {
-  final String hintText;
-  final TextEditingController controller;
-  const EnterCarInfo(
-      {super.key, required this.hintText, required this.controller});
+	final String hintText;
+	final TextEditingController controller;
+	const EnterCarInfo({super.key, required this.hintText, required this.controller});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(bottom: 10, right: 40, left: 30),
-      child: TextField(
-        obscureText: false,
-        controller: controller,
-        maxLines: null,
-        decoration: InputDecoration(
-          filled: true,
-          fillColor: Colors.white,
-          border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(30),
-              borderSide: const BorderSide(color: Colors.red)),
-          hintText: hintText,
-        ),
-      ),
-    );
-  }
+	@override
+	Widget build(BuildContext context) {
+		return Container(
+			padding: const EdgeInsets.only(bottom: 10, right: 40, left: 30),
+			child: TextField(
+				obscureText: false,
+				controller: controller,
+				maxLines: null,
+				decoration: InputDecoration(
+					filled: true,
+					fillColor: Colors.white,
+					border: OutlineInputBorder(
+						borderRadius: BorderRadius.circular(30),
+						borderSide: const BorderSide(color: Colors.red)
+					),
+					hintText: hintText,
+				),
+			),
+		);
+	}
 }
